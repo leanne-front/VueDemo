c105afb654ac0c771ade6283c33e562c7d1d72b2
leanne_front <leanne_front@cxgi.org>
2021-01-20 14:25:19 +0800

    finished template and basic styling

--- Vue/vue-weather-demo/src/App.vue
+++ Vue/vue-weather-demo/src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <div id="app">
+    <div class="container">
+      <div class="search-box">
+        <input type="text" placeholder="Key in Country..." class="search-bar" />
+      </div>
+
+      <div class="weather-wrapper">
+        <div class="location-box">
+          <div class="location">Taipei</div>
+          <div class="date">January 18 2021</div>
+        </div>
+        <div class="weather-box">
+          <div class="temperature">26°C</div>
+          <div class="weather">Cloud</div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -16,12 +33,97 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: 50%;
+  transition: 0.5s;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  margin: 10px;
+}
+
+#app.warm{
+  background-image: url('./assets/warm-bg.jpg');
+}
+.container {
+  height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  )
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 35px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  transition: all 0.4s ease-in;
+  border-radius: 0 16px 0 16px;
+  font-size: 20px;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: hsla(0,0%,100%,.5);
+}
+.search-box .search-bar:focus {
+  background-color: rgba(255, 255, 255, 0.75);
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+.weather-box {
+  text-align: center;
+  margin-top: 15px;
+}
+
+.weather-box .temperature {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  font-size: 48px;
+  color: #fff;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
