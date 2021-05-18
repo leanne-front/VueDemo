468a5ee5e1c454c89089855a47b99d5280a9a7b0
leanne_front <leanne_front@cxgi.org>
2021-05-18 11:16:01 +0800

    0518 after add

---
+++ _L-personal/Vue-project/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,58 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }} </h2>
+        <button @click="toggleFavorite(id)">Favorite it</button>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li>
+                <strong>Phone:</strong>
+                {{ phone }}
+            </li>
+            <li>
+                <strong>Email:</strong>
+                {{ email }}
+            </li>
+            <button @click="$emit('delete')">Delete</button>
+        </ul>
+        
+    </li>
+</template>
+
+<script>
+export default {
+props:{
+    // "friends": {
+    //   type: Object
+    // },
+    id:{
+        type:String
+    }, 
+    name:{
+        type:String
+    }, 
+    phone:{
+        type:String
+    }, 
+    email:{
+        type:String
+    }, 
+    isFavorite:{
+        type: Boolean
+    }
+},
+data() {
+    return {
+    isFavoriteInApp: false,
+    detailsAreVisible: false
+    };
+},
+methods: {
+    toggleDetails() {
+    this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite(id) {
+    this.$emit('toggleEvent', id);
+    }
+}
+};
+</script>
